2083d2d66d148e0fdc58a45bf6972e7f59576453
lotteRavn <lotteravn@lottes-mac-mini.local>
2016-05-12 11:28:07 +0200

    Connecting Menu

--- Bournonville'sWildWestTest/OurViewController.swift
+++ Bournonville'sWildWestTest/OurViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class OurViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+        menuButton.target = self.revealViewController()
+        menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
 
         // Do any additional setup after loading the view.
     }
